84b9c256c7a06fae4413f5d1c024dcb1f65aeb54
Atilla Saadat <atillaabc@gmail.com>
2020-07-17 18:02:14 -0400

    made data handling changes, fiyin to fix more

--- main_script.py
+++ main_script.py
@@ -14,51 +14,33 @@ from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
 from sklearn.pipeline import Pipeline
+
+from IPython import embed
 #Change Directory if needed
 
 basepath = "./Csv/Outputs"
-li = []
-#Read the csv files 
-def readCSV(csv):
-    return pd.read_csv(csv, engine = 'python')
+li2 = []
+inputScenarios = []
 
 #Read through every CSV file but only use the columns that have faulty data
 print('Reading Started!')
 with os.scandir(basepath) as entries:
     for entry in entries:
-        
         if entry.is_file():
-            
             df = pd.read_csv(entry,usecols=[16,17,18,20,21,22])
-            li.append(df)
-            
+            x_scaled = preprocessing.MinMaxScaler().fit_transform(df)
+            li2.append(x_scaled)
+            inputsScenarios.appned(int(entry.name.split('_')[1]))
+
 
 print('Reading Done!')
 #Concatenate to a datafrme and read from that dataframe
 
-from sklearn import preprocessing
-
-
-li2 =[]
-
-for x in li:
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    li2.append(x_scaled)
-    
 li2 = np.vstack(li2)
-             
 df1 = pd.DataFrame(li2,columns=['q1_faulty','q2_faulty','q3_faulty','o1_faulty','o2_faulty','o3_faulty']).to_csv('./test.csv', index = False, header=True)           
 df2 = pd.read_csv('./test.csv',chunksize=601)  
-  
-with open("./Csv/Inputs/adcs_fdi_inputs_625.csv", "r") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    scenario =[]
-    simulation = []
-
-    for lines in csv_reader:
-      scenario.append(lines[1])
       
       
 scenario.remove('scenario')
@@ -85,14 +67,9 @@ for chunk in df2:
     Skew.append(chunk.skew())
     SEM.append(stats.sem(chunk))
     MSD.append(stats.median_absolute_deviation(chunk))
-    def rMS(chunk): #calculate root mean square
-        rms = np.sqrt(np.mean(chunk**2))
-        return rms
-    RMS.append(rMS(chunk))
-    
-    def crest_factor(chunk): #calculate crest factor
-        return np.max(np.abs(chunk))/np.sqrt(np.mean(np.square(chunk)))
-    Crst.append(crest_factor(chunk))
+    RMS.append(np.sqrt(np.mean(chunk**2)))
+    cf = np.max(np.abs(chunk))/np.sqrt(np.mean(np.square(chunk)))
+    Crst.append(cf)
 
 #Place the list of calculated values into a dataframe     
 df_mean = pd.DataFrame(Mean)
@@ -115,6 +92,12 @@ df_crst.columns = ['CRSTQ1','CRSTQ2','CRSTQ3','CRSTO1','CRSTO2','CRSTO3']
 df_MSD = pd.DataFrame(MSD)
 df_MSD.columns = ['MSDQ1','MSDQ2','MSDQ3','MSDO1','MSDO2','MSDO3']
 #Open the input csv file and take the scenario column only 
+
+# [ idea x 6 ]
+# [ . ]
+# [ . ]
+# [ N (num of datasets in ID order) ]
+# N x (idea x 6 ) array
 
 data = pd.concat([df_mean,df_sd,df_kurt,df_skew,df_var,df_sem,df_rms,df_crst,df_MSD],axis=1)
 # data.drop(data.tail(4).index,inplace=True)
@@ -158,26 +141,26 @@ from sklearn.pipeline import Pipeline
 # define dataset
 # get a list of models to evaluate
 # def get_models():
-# 	models = dict()
-# 	for i in range(2, 10):
-# 		rfe = RFE(estimator=DecisionTreeClassifier(), n_features_to_select=i)
-# 		model = DecisionTreeClassifier()
-# 		models[str(i)] = Pipeline(steps=[('s',rfe),('m',model)])
-# 	return models
+#   models = dict()
+#   for i in range(2, 10):
+#       rfe = RFE(estimator=DecisionTreeClassifier(), n_features_to_select=i)
+#       model = DecisionTreeClassifier()
+#       models[str(i)] = Pipeline(steps=[('s',rfe),('m',model)])
+#   return models
  
 # # evaluate a give model using cross-validation
 # def evaluate_model(model):
-# 	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=0)
-# 	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
-# 	return scores
+#   cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=0)
+#   scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+#   return scores
 # models = get_models()
 # # evaluate the models and store results
 # results, names = list(), list()
 # for name, model in models.items():
-# 	scores = evaluate_model(model)
-# 	results.append(scores)
-# 	names.append(name)
-# 	print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+#   scores = evaluate_model(model)
+#   results.append(scores)
+#   names.append(name)
+#   print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
 # # plot model performance for comparison
 # plt.boxplot(results, labels=names, showmeans=True)
 # plt.show()
@@ -262,7 +245,7 @@ plt.savefig('./Model_Loss.svg')
 plt.show()
 
 def summarize_results(score):
-	print(score)
-	m, s = mean(score), std(score)
-	print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+    print(score)
+    m, s = mean(score), std(score)
+    print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
     
