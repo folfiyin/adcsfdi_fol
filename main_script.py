d24533348fd71736b7a301a60321f33cf21e7567
Fiyin Folami <folami@uwindsor.ca>
2020-08-08 22:35:59 -0400

    reduced dataset

--- main_script.py
+++ main_script.py
@@ -6,6 +6,7 @@ import os
 from scipy.stats import entropy
 from scipy import stats
 from sklearn import metrics
+from sklearn.preprocessing import StandardScaler,normalize,Normalizer
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -15,6 +16,17 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 from sklearn.pipeline import Pipeline
+import keras
+from keras.layers.embeddings import Embedding
+
+from keras.models import Sequential
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, SpatialDropout1D
+from keras.layers import Conv1D, MaxPooling1D
+from keras import backend as K
+from keras.utils import np_utils
+from keras.layers import LSTM
+
 
 from IPython import embed
 #Change Directory if needed
@@ -30,8 +42,8 @@ with os.scandir(basepath) as entries:
     for entry in entries:
         if entry.is_file():
             df = pd.read_csv(entry,usecols=[16,17,18,20,21,22])
-            x_scaled = preprocessing.MinMaxScaler().fit_transform(df)
-            li2.append(x_scaled)
+            # x_scaled = preprocessing.MinMaxScaler().fit_transform(df)
+            li2.append(df)
             inputScenarios.append(int(entry.name.split('_')[1]))
 
 
@@ -39,10 +51,11 @@ print('Reading Done!')
 #Concatenate to a datafrme and read from that dataframe
 
 li2 = np.vstack(li2)
-df1 = pd.DataFrame(li2,columns=['q1_faulty','q2_faulty','q3_faulty','o1_faulty','o2_faulty','o3_faulty']).to_csv('./test.csv', index = False, header=True)           
+df1 = pd.DataFrame(li2).to_csv('./test.csv', index = False, header=True)           
 df2 = pd.read_csv('./test.csv',chunksize=601)  
 scenario = pd.DataFrame(inputScenarios)
 scenario.columns=['scenario']
+
          
 Mean=[]
 SD=[]
@@ -70,240 +83,103 @@ for chunk in df2:
 
 #Place the list of calculated values into a dataframe     
 df_mean = pd.DataFrame(Mean)
-df_mean.columns = ['MeanQ1', 'MeanQ2', 'MeanQ3','MeanO1','MeanO2','MeanO3']
 df_sd = pd.DataFrame(SD)
-df_sd.columns = ['SdQ1','SdQ2','SdQ3','SdO1','SdO2','SdO3']
 df_kurt = pd.DataFrame(Kurt)
-df_kurt.columns = ['KQ1','KQ2','KQ3','KO1','KO2','KO3']
 df_skew = pd.DataFrame(Skew)
-df_skew.columns = ['SkQ1','SkQ2','SkQ3','SkO1','SkO2','SkO3']
 df_var = pd.DataFrame(Var)
-df_var.columns = ['VarQ1','VarQ2','VarQ3','VarO1','VarO2','VarO3'] 
-  
 df_sem = pd.DataFrame(SEM)
-df_sem.columns = ['SemQ1','SemQ2','SemQ3','SemO1','SemO2','SemO3']
 df_rms = pd.DataFrame(RMS)
-df_rms.columns = ['RMSQ1','RMSQ2','RMSQ3','RMSO1','RMSO2','RMSO3']
 df_crst = pd.DataFrame(Crst)
-df_crst.columns = ['CRSTQ1','CRSTQ2','CRSTQ3','CRSTO1','CRSTO2','CRSTO3']
 df_MSD = pd.DataFrame(MSD)
-df_MSD.columns = ['MSDQ1','MSDQ2','MSDQ3','MSDO1','MSDO2','MSDO3']
-#Open the input csv file and take the scenario column only 
 
-# [ idea x 6 ]
-# [ . ]
-# [ . ]
-# [ N (num of datasets in ID order) ]
-# N x (idea x 6 ) array
 
 data = pd.concat([df_mean,df_sd,df_kurt,df_skew,df_var,df_sem,df_rms,df_crst,df_MSD],axis=1)
-# data.drop(data.tail(4).index,inplace=True)
-database = pd.concat([data,scenario],axis=1)     
-      
-print(database)
 
-# base=[]
-# # SD=[]
-# # Kurt=[]
-# # Skew=[]
-# # Var=[]
-# # SEM=[]
-# # RMS=[]
-# # Crst=[]
-# # MSD = []
+df3 = pd.concat([data,scenario],axis=1)   
 
-# # #For the chunks of data in dataframe 2, loop through and append each statistical feature to a list
+database = pd.DataFrame(df3)
 
-# for x in df1.itertuples():
-#     base.append(np.mean(x,axis=0))
-#     base.append(np.std(x,axis=0))
-#     base.append(np.var(x,axis=0))
-#     base.append(df1.kurtosis())
-#     base.append(df1.skew())
-#     base.append(stats.sem(x,axis=0))
-#     base.append(stats.median_absolute_deviation(x,axis=0))
-#     # base.append(np.sqrt(np.mean(x**2)))
-#     # cf = np.max(np.abs(x))/np.sqrt(np.mean(np.square(x)))
-#     # base.append(cf)
-        
-
-# df2 = pd.DataFrame(base)
+dataframe = database[database.scenario != 0]
 
 
-# #Place the list of calculated values into a dataframe     
-# df_mean = pd.DataFrame(Mean)
-# df_mean.columns = ['MeanQ1', 'MeanQ2', 'MeanQ3','MeanO1','MeanO2','MeanO3']
-# df_sd = pd.DataFrame(SD)
-# df_sd.columns = ['SdQ1','SdQ2','SdQ3','SdO1','SdO2','SdO3']
-# df_kurt = pd.DataFrame(Kurt)
-# df_kurt.columns = ['KQ1','KQ2','KQ3','KO1','KO2','KO3']
-# df_skew = pd.DataFrame(Skew)
-# df_skew.columns = ['SkQ1','SkQ2','SkQ3','SkO1','SkO2','SkO3']
-# df_var = pd.DataFrame(Var)
-# df_var.columns = ['VarQ1','VarQ2','VarQ3','VarO1','VarO2','VarO3'] 
-  
-# df_sem = pd.DataFrame(SEM)
-# df_sem.columns = ['SemQ1','SemQ2','SemQ3','SemO1','SemO2','SemO3']
-# df_rms = pd.DataFrame(RMS)
-# df_rms.columns = ['RMSQ1','RMSQ2','RMSQ3','RMSO1','RMSO2','RMSO3']
-# df_crst = pd.DataFrame(Crst)
-# df_crst.columns = ['CRSTQ1','CRSTQ2','CRSTQ3','CRSTO1','CRSTO2','CRSTO3']
-# df_MSD = pd.DataFrame(MSD)
-# df_MSD.columns = ['MSDQ1','MSDQ2','MSDQ3','MSDO1','MSDO2','MSDO3']
-# #Open the input csv file and take the scenario column only 
-
-# # [ idea x 6 ]
-# # [ . ]
-# # [ . ]
-# # [ N (num of datasets in ID order) ]
-# # N x (idea x 6 ) array
-
-# data = pd.concat([df_mean,df_sd,df_kurt,df_skew,df_var,df_sem,df_rms,df_crst,df_MSD],axis=1)
-# # data.drop(data.tail(4).index,inplace=True)
-
-# # # df_zscore = (data - data.mean())/data.std()
-
-# database = pd.concat([data],axis=1)
-# # database.drop(database.tail(13).index,inplace=True)
-# # print(database)
-
-# from sklearn.model_selection import train_test_split,cross_val_score,KFold
-# # # from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-# # # from sklearn.preprocessing import StandardScaler
 
+#Define dataset 
+X = dataframe.iloc[0:2359,0:54].values 
+y = dataframe.iloc[0:2359,54]
 
-
-X = database.iloc[:,0:54].values 
-y = database.iloc[:,54]
 
 encoder = LabelEncoder()
-encoder.fit(y)
-encoded_Y = encoder.transform(y)
+encoded_Y = encoder.fit_transform(y)
 # convert integers to dummy variables (i.e. one hot encoded)
 dummy_y = np_utils.to_categorical(encoded_Y)
 
+
+
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.multiclass import OneVsRestClassifier
+
+#  # define model
+# model = LogisticRegression()
+# # define the ovr strategy
+# ovr = OneVsRestClassifier(model)
+# # fit model
+# ovr.fit(X, y)
+# # make predictions
+# yhat = ovr.predict(X)
+
 from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X,encoded_Y, test_size=0.2, stratify=y,random_state=0)
+
+X_train, X_test, y_train, y_test = train_test_split(X,dummy_y, test_size=0.3, stratify=y,random_state=0)
 
 
-# from numpy import mean
-# from numpy import std
-# from sklearn.datasets import make_classification
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import RepeatedStratifiedKFold
-# from sklearn.feature_selection import RFE
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.pipeline import Pipeline
-# # define dataset
-# # get a list of models to evaluate
-# # def get_models():
-# #  	models = dict()
-# #  	for i in range(2, 10):
-# #          rfe = RFE(estimator=DecisionTreeClassifier(), n_features_to_select=i)
-# #          model = DecisionTreeClassifier()
-# #          models[str(i)] = Pipeline(steps=[('s',rfe),('m',model)])
-# #  	return models
- 
-# # # evaluate a give model using cross-validation
-# # def evaluate_model(model):
-# #  	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=0)
-# #  	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
-# #  	return scores
-# # models = get_models()
-# # # evaluate the models and store results
-# # results, names = list(), list()
-# # for name, model in models.items():
-# #  	scores = evaluate_model(model)
-# #  	results.append(scores)
-# #  	names.append(name)
-# #  	print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
-# # # plot model performance for comparison
-# # plt.boxplot(results, labels=names, showmeans=True)
-# # plt.show()
+# X_train = X_train.reshape((X_train.shape[0], X_train.shape[1],1))
+# X_test = X_test.reshape((X_test.shape[0], X_test.shape[1],1))
 
-import keras
-from keras.models import Sequential
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, SpatialDropout1D
-from keras.layers import Conv1D, MaxPooling1D
-from keras import backend as K
-from keras.utils import np_utils
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
-from keras.layers import LSTM
-from sklearn.preprocessing import StandardScaler
-from time import time
 
-X_train, X_test, y_train, y_test = train_test_split(X,dummy_y, test_size=0.2, stratify=y,random_state=0)
+from sklearn import preprocessing
 
-num_classes = 16
- 
-sc = StandardScaler()
-
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-X_train = X_train.reshape((X_train.shape[0], X_train.shape[1],1))
-X_test = X_test.reshape((X_test.shape[0], X_test.shape[1],1))
+from keras.optimizers import SGD
 
 model = Sequential()
-model.add(Conv1D(filters=16, kernel_size=3, input_shape=(55, 1)))
-# model.add(MaxPooling1D(pool_size=5))
-model.add(SpatialDropout1D(0.5))
-model.add(LSTM(30, dropout=0.3, recurrent_dropout=0.3))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(1000, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dropout(0.1))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(y_train.shape[1], activation='sigmoid'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='binary_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
 
-history = model.fit(X_train, y_train,validation_split=0.3, batch_size=50, epochs=100)
-score = model.evaluate(X_test, y_test, batch_size=300, verbose=1)
+history = model.fit(X_train, y_train, validation_split=0.3, epochs=100, batch_size=500)
+accuracy = model.evaluate(X_test, y_test, batch_size=500, verbose=0)
+
+preds = model.predict(X_test)
+preds[preds>=0.5] = 1
+preds[preds<0.5] = 0
+
 y_pred = model.predict(X_test)
-
-y_pred = np.argmax(y_pred, axis=0)
-y_test = np.argmax(y_test, axis=0)
+y_pred = np.argmax(y_pred, axis=1)
+y_test = np.argmax(y_test, axis=1)
 
 metrics.f1_score(y_test, y_pred, average='weighted', labels=np.unique(y_pred))
 
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig(r'C:\Users\fiyin\OneDrive\Documents\project\Model_Acc.svg') 
+plt.show()            
 
-# # # Making the Confusion Matrix and Classification report 
-# x_axis_labels = [1]
-# y_axis_labels = [1]
-# cm = confusion_matrix(y_test, y_pred,labels=np.unique(y_pred),normalize='true')
-# print(cm)
-# print('Classification Report\n',classification_report(y_test, y_pred))
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper right')
+plt.savefig(r'C:\Users\fiyin\OneDrive\Documents\project\Model_Loss.svg') 
+plt.show()
 
-# import seaborn as sn
-# sn.set(font_scale=1) # for label size
-# sn.heatmap(cm, annot=True,cbar=False, xticklabels=x_axis_labels,yticklabels=y_axis_labels, annot_kws={"size": 16},fmt=".1f") # font size
-# plt.xlabel('Scenarios')
-# plt.ylabel('Scenarios')
-# # plt.show()
-# plt.savefig(r'C:\Users\fiyin\OneDrive\Documents\project\Confusion_matrix.svg')
-# plt.show()
-
-# plt.plot(history.history['val_accuracy'])
-# plt.title('Model accuracy')
-# plt.ylabel('Accuracy')
-# plt.xlabel('Epoch')
-# plt.legend(['Train', 'Test'], loc='upper left')
-# plt.savefig(r'C:\Users\fiyin\OneDrive\Documents\project\Model_Acc.svg') 
-# plt.show()            
-
-# # Plot training & validation loss values
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('Model loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-# plt.legend(['Train', 'Test'], loc='upper right')
-# plt.savefig(r'C:\Users\fiyin\OneDrive\Documents\project\Model_Loss.svg') 
-# plt.show()
-
-# def summarize_results(score):
-# 	print(score)
-# 	m, s = mean(score), std(score)
-# 	print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
